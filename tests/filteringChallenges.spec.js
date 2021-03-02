1ad50ffdfc575705ac8f8669a068752f36486fea
Bruno Batista <brunobatista101@gmail.com>
2021-03-02 09:36:01 -0300

    voltando versao anterior

--- tests/filteringChallenges.spec.js
+++ tests/filteringChallenges.spec.js
@@ -1,4 +1,4 @@
-const fs = require('fs'); 
+const { readFileSync } = require('fs');
 const Importer = require('./importer');
 
 describe('Desafios sobre filtragem de dados', () => {
@@ -21,7 +21,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('9 - Mostre todos os valores de `notes` da tabela `purchase_orders` que não são nulos.', () => {
     it('Verifica o desafio9', async () => {
-      const challengeQuery = fs.readFileSync('./desafio9.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio9.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult9');
 
       expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
@@ -30,7 +30,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('10 - Mostre todos os dados da tabela `purchase_orders` em ordem decrescente ordenados por `created_by` em que o `created_by` é maior ou igual a 3. E como critério de desempate para a ordenação, ordene também os resultados pelo `id` de forma crescente.', () => {
     it('Verifica o desafio10', async () => {
-      const challengeQuery = fs.readFileSync('desafio10.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio10.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult10');
 
       expect(
@@ -50,7 +50,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('12 - Mostre as `submitted_date` de `purchase_orders` em que a `submitted_date` é do dia 26 de abril de 2006.', () => {
     it('Verifica o desafio12', async () => {
-      const challengeQuery = fs.readFileSync('desafio12.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio12.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult12');
 
       expect(
@@ -61,7 +61,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('13 - Mostre o `supplier_id` das `purchase_orders` em que o `supplier_id` seja 1 ou 3.', () => {
     it('Verifica o desafio13', async () => {
-      const challengeQuery = fs.readFileSync('desafio13.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio13.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult13');
 
       expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
@@ -70,7 +70,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('14 - Mostre os `supplier_id` da `purchase_orders` em que o `supplier_id` seja de 1 a 3, incluindo tanto o 1 quanto o 3.', () => {
     it('Verifica o desafio14', async () => {
-      const challengeQuery = fs.readFileSync('desafio14.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio14.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult14');
 
       expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
@@ -79,7 +79,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('15 - Mostre somente as horas (sem os minutos e os segundos) da `submitted_date` de todos registros de `purchase_orders`. Chame essa coluna de `submitted_hour`.', () => {
     it('Verifica o desafio15', async () => {
-      const challengeQuery = fs.readFileSync('desafio15.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio15.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult15');
 
       expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
@@ -88,7 +88,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('16 - Exiba a `submitted_date` das `purchase_orders` que estão entre `2006-01-26 00:00:00` e `2006-03-31 23:59:59`.', () => {
     it('Verifica o desafio16', async () => {
-      const challengeQuery = fs.readFileSync('desafio16.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio16.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult16');
 
       expect(
@@ -99,7 +99,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('17 - Mostre os registros das colunas `id` e `supplier_id` das `purchase_orders` em que os `supplier_id` sejam tanto 1, ou 3, ou 5, ou 7.', () => {
     it('Verifica o desafio17', async () => {
-      const challengeQuery = fs.readFileSync('desafio17.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio17.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult17');
 
       expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
@@ -108,7 +108,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('18 - Mostre todos os registros de `purchase_orders` que tem o `supplier_id` igual a 3 e `status_id` igual a 2.', () => {
     it('Verifica o desafio18', async () => {
-      const challengeQuery = fs.readFileSync('desafio18.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio18.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult18');
 
       expect(
@@ -119,7 +119,7 @@ describe('Desafios sobre filtragem de dados', () => {
 
   describe('19 - Mostre a quantidade de pedidos que foram feitos na tabela `orders` pelo `employee_id` igual a 5 ou 6, e que foram enviados através do método `shipper_id` igual a 2. Chame a coluna de orders_count.', () => {
     it('Verifica o desafio19', async () => {
-      const challengeQuery = fs.readFileSync('desafio19.sql', 'utf8').trim();
+      const challengeQuery = readFileSync('desafio19.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult19');
 
       expect(await sequelize.query(challengeQuery, { type: 'SELECT' })).toEqual(expectedResult);
