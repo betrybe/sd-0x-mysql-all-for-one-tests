3130714dee5a2293be592c3b695c6857d8a18e63
Bruno Batista <brunobatista101@gmail.com>
2021-03-02 10:18:08 -0300

    teste2

--- tests/initialChallenges.spec.js
+++ tests/initialChallenges.spec.js
@@ -11,12 +11,11 @@ describe('Desafios iniciais', () => {
     );
 
     await importer.import('./northwind.sql');
-
     importer.disconnect();
+  });
 
-    sequelize = new Sequelize(
-      `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.HOSTNAME}:3306/northwind`
-    );
+  beforeEach(async () => {
+    await sequelize.query('USE northwind;', { type: 'RAW' });
   });
 
   afterAll(async () => {
@@ -24,7 +23,7 @@ describe('Desafios iniciais', () => {
     sequelize.close();
   });
 
-  describe('Exiba apenas os nomes do produtos na tabela `products`', () => {
+  describe('1 - Exiba apenas os nomes do produtos na tabela `products`.', () => {
     it('Verifica o desafio1', async () => {
       const challengeQuery = readFileSync('desafio1.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult1');
@@ -33,7 +32,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Exiba os dados de todas as colunas da tabela `products`', () => {
+  describe('2 - Exiba os dados de todas as colunas da tabela `products`.', () => {
     it('Verifica o desafio2', async () => {
       const challengeQuery = readFileSync('desafio2.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult2');
@@ -42,7 +41,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Escreva uma query que exiba os valores da coluna que representa a primary key da tabela `products`', () => {
+  describe('3 - Escreva uma query que exiba os valores da coluna que representa a primary key da tabela `products`.', () => {
     it('Verifica o desafio3', async () => {
       const challengeQuery = readFileSync('desafio3.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult3');
@@ -51,7 +50,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Conte quantos registros existem em `product_name` de `products`', () => {
+  describe('4 - Conte quantos registros existem em `product_name` de `products`.', () => {
     it('Verifica o desafio4', async () => {
       const challengeQuery = readFileSync('desafio4.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult4');
@@ -60,7 +59,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Monte uma query que exiba os dados da tabela `products` a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro', () => {
+  describe('5 - Monte uma query que exiba os dados da tabela `products` a partir do quarto registro até o décimo terceiro, incluindo tanto um quanto o outro. Obs.: não use `where` ou `order by`.', () => {
     it('Verifica o desafio5', async () => {
       const challengeQuery = readFileSync('desafio5.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult5');
@@ -69,7 +68,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Exiba os dados das colunas `product_name` e `id` da tabela `products` de maneira que os resultados estejam em ordem alfabética dos nomes', () => {
+  describe('6 - Exiba os dados das colunas `product_name` e `id` da tabela `products` de maneira que os resultados estejam em ordem alfabética dos nomes.', () => {
     it('Verifica o desafio6', async () => {
       const challengeQuery = readFileSync('desafio6.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult6');
@@ -78,7 +77,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Mostre apenas os ids dos 5 últimos registros da tabela `products` (a ordernação deve ser baseada na coluna `id`)', () => {
+  describe('7 - Mostre apenas os ids dos 5 últimos registros da tabela `products` (a ordernação deve ser baseada na coluna `id`).', () => {
     it('Verifica o desafio7', async () => {
       const challengeQuery = readFileSync('desafio7.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult7');
@@ -87,7 +86,7 @@ describe('Desafios iniciais', () => {
     });
   });
 
-  describe('Faça uma consulta que retorne três colunas contendo os nomes `A`, `Trybe` e `eh` com os valores `5 + 6`, `"de"` e `2 + 8`, respectivamente', () => {
+  describe('8 - Faça uma consulta que retorne três colunas. Na primeira coluna, exiba a soma de `5 + 6` (essa soma deve ser realizada pelo SQL). Na segunda coluna deve haver a palavra \"de\". E por fim, na terceira coluna, exiba a soma de `2 + 8` (essa soma deve ser realizada pelo SQL). A primeira coluna deve se chamar \"A\", a segunda coluna deve se chamar \"Trybe\" e a terceira coluna deve se chamar \"eh\". Não use colunas pre-existentes, apenas o que for criado na hora.', () => {
     it('Verifica o desafio8', async () => {
       const challengeQuery = readFileSync('desafio8.sql', 'utf8').trim();
       const expectedResult = require('./challengesResults/challengeResult8');
